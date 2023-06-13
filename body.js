1d73705cc74802badae2c19dfc66484e34aaa83d
10be <a01024791386@gmail.com>
2023-06-13 17:41:24 +0900

    ver1 commit

--- body.js
+++ body.js
@@ -1,17 +1,14 @@
 //;SSdocument.getElementById("text1").style.fontSize = '17px';
+
+var iron = 0;
+
 function box1on(){
     document.getElementById("box").style.display = 'block';
 }
 function box1off(){
     document.getElementById("box").style.display = 'none';
 }
-function banana(){
-    document.getElementById("text").innerHTML = '포타슘';
-    document.getElementById("queen").style.display = 'none';
-    document.getElementById("nice").style.display = 'block';
-    setTimeout(() => {
-        document.getElementById("text").innerHTML = '크리스 바나나 먹어';
-        document.getElementById("queen").style.display = 'block';
-        document.getElementById("nice").style.display = 'none';
-    }, 2000);
+function irons() {
+    iron += 1
+    document.getElementById("text").innerHTML = iron;
 }
--- head.js
+++ head.js
@@ -2,7 +2,3 @@ function sound() {
     var audio = document.getElementById("sound");
     audio.play();
 }
-function mixtape() {
-    var tape = document.getElementById("mix");
-    tape.play();
-}
